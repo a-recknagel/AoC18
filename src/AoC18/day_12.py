39450e3d41f84149e1c86ad0b2991ecd08840e7d
Arne Recknagel <arne.recknagel@cognotekt.com>
2018-12-12 09:27:46 +0100

    day 12
    they should implement reading comprehension in python asap so I don't
    have to do it by head each time, took my another 20 minutes to get the
    pot-idx-summation rule right

--- src/AoC18/day_12.py
+++ src/AoC18/day_12.py
@@ -1,7 +1,9 @@
+from typing import Tuple
+
 from AoC18 import read_data
 
 
-def plant_generator(data, num_gens):
+def plant_generator(data, num_gens) -> Tuple[int, str, bool]:
     offset = num_gens + 5
     current, last = ('.'*offset) + data[0][15:] + ('.'*offset), ''
     rules = [s[:5] for s in data[2:] if s.endswith('#')]
@@ -16,26 +18,28 @@ def plant_generator(data, num_gens):
                 new[match+2] = '#'
         last = current
         current = ''.join(new)
-    return sum(idx - offset for idx in range(len(current))
-               if current[idx] == '#'), current, last[:-1] == current[1:]
+    pot_sum = sum(idx - offset for idx in range(len(current))
+                  if current[idx] == '#')
+    return pot_sum, current, last[:-1] == current[1:]
+
 
 def one(data, num_gens):
-    ret, _, _ = plant_generator(data, num_gens)
+    ret, *_ = plant_generator(data, num_gens)
     return ret
 
 
 def two(data, num_runs):
-    when_cycled = 50
+    # cycle detection
+    when_cycled = 0
     plants, is_cycle = '', False
     while not is_cycle:
-        _, plants, is_cycle = plant_generator(data, when_cycled)
         when_cycled += 50
+        _, plants, is_cycle = plant_generator(data, when_cycled)
+    # .. or just run it with 'num_gens = 1000' and assume it has cycled by then
 
-    print(when_cycled, plants)
-
-    indices = [idx-(when_cycled+5) for idx in range(len(plants))
-               if plants[idx] == '#']
-    return sum(idx+(num_runs-when_cycled) for idx in indices)
+    pot_indices = [idx - (when_cycled + 5) for idx in range(len(plants))
+                   if plants[idx] == '#']
+    return sum(idx+(num_runs-when_cycled) for idx in pot_indices)
 
 
 if __name__ == '__main__':
