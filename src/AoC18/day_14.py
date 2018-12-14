44ca50dee10ac19c4d545f3d0a655cb23156eea3
Arne Recknagel <arne.recknagel@cognotekt.com>
2018-12-14 09:15:40 +0100

    day 14
    meh

--- src/AoC18/day_14.py
+++ src/AoC18/day_14.py
@@ -1,8 +1,12 @@
+from AoC18 import read_data
+
+
 def digits(num):
     yield from map(int, str(num))
 
 
-def one(num_recipes):
+def one(data):
+    num_recipes = int(data)
     elf_1, elf_2 = 0, 1
     scores = [3, 7]
     while len(scores) <= (num_recipes + 10):
@@ -11,16 +15,36 @@ def one(num_recipes):
             scores.append(digit)
         elf_1 = (elf_1 + scores[elf_1] + 1) % len(scores)
         elf_2 = (elf_2 + scores[elf_2] + 1) % len(scores)
-    print(scores)
-    print()
     return ''.join(map(str, scores[num_recipes:num_recipes+10]))
 
 
-def two():
-    ...
+def two(data):
+    elf_1, elf_2 = 0, 1
+    scores = [3, 7]
+    target = [int(s) for s in data]
+    while True:
+        recipe = scores[elf_1] + scores[elf_2]
+        for digit in digits(recipe):
+            scores.append(digit)
+        if len(scores) < len(target):
+            continue
+        for i in range(len(target)):
+            if scores[-(i+1)] != target[-(i+1)]:
+                break
+        else:
+            return len(scores) - 5
+        for i in range(len(target)):
+            if scores[-(i+2)] != target[-(i+1)]:
+                break
+        else:
+            return len(scores) - 7  # ... why 7 and not 6?
+        elf_1 = (elf_1 + scores[elf_1] + 1) % len(scores)
+        elf_2 = (elf_2 + scores[elf_2] + 1) % len(scores)
 
 
 if __name__ == '__main__':
-    inp = 681901
+    inp = read_data('day_14.txt')[0]
     print(one(inp))
-    two()
+    print(two(inp))
+
+
